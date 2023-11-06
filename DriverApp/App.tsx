57d38030aeca2306ced34c65ca9232fe0251ed9e
sulochan <sulochankhadka25@gmail.com>
2023-11-06 07:47:04 +0530

    Car assign

--- DriverApp/App.tsx
+++ DriverApp/App.tsx
@@ -6,11 +6,50 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import {Amplify, API, Auth, graphqlOperation} from 'aws-amplify';
+import awsExports from './src/aws-exports';
+Amplify.configure(awsExports);
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from '@aws-amplify/ui-react-native';
+import {getCarId} from './src/graphql/queries';
+import {createCar} from './src/graphql/mutations';
 
 const App: () => React$Node = () => {
+  // Auth.signOut();
+  useEffect(() => {
+    const updateUserCar = async () => {
+      // console.log('This is the test log');
+      try {
+        const authenticatedUser = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+        console.log(authenticatedUser);
+        if (!authenticatedUser) return;
+        const carData = await API.graphql(
+          graphqlOperation(getCarId, {id: authenticatedUser.attributes.sub}),
+        );
+        console.log(carData);
+        if (carData?.data?.getCar) {
+          console.log('User already has a car assigned');
+          return;
+        }
+        const newCar = {
+          id: authenticatedUser.attributes.sub,
+          type: 'Basic',
+          carUserId: authenticatedUser.attributes.sub,
+        };
+        await API.graphql(graphqlOperation(createCar, {input: newCar}));
+      } catch (error) {
+        console.log('The error is :', error);
+      }
+    };
+    updateUserCar();
+  }, []);
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -21,4 +60,4 @@ const App: () => React$Node = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
---
+++ DriverApp/src/graphql/mutations.js
@@ -0,0 +1,19 @@
+export const createCar = /* GraphQL */ `
+  mutation CreateCar(
+    $input: CreateCarInput!
+    $condition: ModelCarConditionInput
+  ) {
+    createCar(input: $input, condition: $condition) {
+      id
+      type
+      latitude
+      longitude
+      heading
+      isActive
+      createdAt
+      updatedAt
+      carUserId
+      __typename
+    }
+  }
+`;
